ead79e11efcda8a729de1a395d4cbdd81e474b00
Mike Irvin <irvinm@yahoo.com>
2024-03-09 16:58:37 -0500

    Initial working commit

---
+++ background.js
@@ -0,0 +1,26 @@
+let hideTabBar = JSON.parse(localStorage.getItem('hideTabBar')) || false;
+
+function updateIcon() {
+    let path = hideTabBar ? 'icon-hidden.png' : 'icon-visible.png';
+    browser.browserAction.setIcon({ path: path });
+}
+
+browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+        browser.tabs.create({ url: 'options.html' });
+    }
+});
+
+browser.browserAction.onClicked.addListener((tab) => {
+    browser.windows.getAll().then((windows) => {
+        let titlePreface = hideTabBar ? "" : "XXX ";
+        windows.forEach((window) => {
+            browser.windows.update(window.id, { titlePreface: titlePreface });
+        });
+        hideTabBar = !hideTabBar;
+        localStorage.setItem('hideTabBar', JSON.stringify(hideTabBar));
+        updateIcon();
+    });
+});
+
+updateIcon();
