f4cb24bef0c37263a11cfa6a78a382ab47f2c728
Mike Irvin <irvinm@yahoo.com>
2024-11-03 05:34:46 -0500

    Hard code formatting & spans for CSS blocks to get around "innerHTML" usage in prism.js
    When submitting addon to AMO, received the automatic review warning: "Unsafe assignment to innerHTML".  Need to fix a workaround to this if want to go back to dynamic usage of CSS formatting.

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,3 +1,3 @@
 del ToggleNativeTabBar.xpi
-"C:\Program Files\WinRAR\winrar.exe" a ToggleNativeTabBar.zip prism.css prism.js background.js manifest.json icon-hidden.png icon-visible.png menu.png MinMaxClose.png options.js options.html options.css
+"C:\Program Files\WinRAR\winrar.exe" a ToggleNativeTabBar.zip prism.css background.js manifest.json icon-hidden.png icon-visible.png menu.png MinMaxClose.png options.js options.html options.css
 ren ToggleNativeTabBar.zip ToggleNativeTabBar.xpi
