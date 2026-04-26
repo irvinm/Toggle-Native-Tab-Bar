c93815a9896e91c9f8c27cd903b2a1a7dbbee26a
Mike Irvin <irvinm@yahoo.com>
2026-04-26 08:17:40 -0400

    Remove unneeded files

--- MakeXPI.bat
+++
@@ -1,3 +0,0 @@
-del ToggleNativeTabBar.xpi
-"C:\Program Files\WinRAR\winrar.exe" a ToggleNativeTabBar.zip prism.css background.js manifest.json icon-hidden.png icon-visible.png native-buttons.png menu.png MinMaxClose.png options.js options.html options.css
-ren ToggleNativeTabBar.zip ToggleNativeTabBar.xpi
